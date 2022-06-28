bde6d734c9f2fa3d3aa1fc7ccba46e07852db369
Zayedislam <islamzayed75@gmail.com>
2022-06-28 11:39:37 +0600

    upload project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Admin/Home/Home";
+import SignIn from "./components/Auth/SignIn/SignIn";
+import SignUp from "./components/Auth/SignUp/SignUp";
+import Layout from "./components/Layouts/Layout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/home" element={<Layout></Layout>} />
+          {/* Auth Routes */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+     
+    </>
   );
 }
 
---
+++ src/components/Admin/Home/Home.js
@@ -0,0 +1,15 @@
+import { Alert } from "react-bootstrap";
+import Layout from "../../Layouts/Layout";
+const Home = () => {
+  return (
+    <>
+      <Layout>
+        <Alert variant="success">
+          <h1 className="text text-center">WelCome to Admin Dashboard</h1>
+        </Alert>
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
